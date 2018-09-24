337f241cdfa3470ad26dd6b0b456be0d717cb8ad
Tunvir Rahman <tunvir.rahman@infolytx.com>
2018-09-24 16:45:14 +0600

    Revert "FIREBASE  facebook Authentication  "
    This reverts commit 016d02350759bfd648925c51dc89de99234f4fcb.

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { LoginButton, AccessToken } from 'react-native-fbsdk';
+'use_strict';
+import React from 'react';
+import {Text} from 'react-native';
 
-export default class App extends Component{
-  render() {
-    return (
-      <View>
-        <LoginButton
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                console.log("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                console.log("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    console.log(data.accessToken.toString())
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => console.log("logout.")}/>
-      </View>
-    );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <Text>Hello world</Text>
+    )
   }
 }
