589ce18fed55d41f3743959f4d84aa71209873a0
tunvir_rahman <tunvir_rahman@yahoo.com>
2019-03-11 22:48:18 +0600

    Navigation Flow added.

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text,SafeAreaView} from 'react-native';
-import {createStackNavigator,createDrawerNavigator} from "react-navigation";
+import {createStackNavigator,createDrawerNavigator,createAppContainer} from "react-navigation";
+import LoginIn from "./src/loginScreen";
+import SignUp from "./src/SignUpScreen";
+import ForgotPassword from "./src/ForgetPasswordScreen";
 
+const AuthFlow = createStackNavigator({
+  Login:LoginIn,
+  SignUp:SignUp,
+  ForgotPassword:ForgotPassword
+},{
+  
+})
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <SafeAreaView style = {{flex:1}}>
-        <View style = {{flex:1,backgroundColor:"white"}}>
-          <Text>Sample Text</Text>
-        </View>
-      </SafeAreaView>
-    );
-  }
-}
+export default createAppContainer(AuthFlow)
--- src/loginScreen.js
+++ src/loginScreen.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,SafeAreaView } from 'react-native';
+import {Button} from "react-native-elements";
 
 export default class LoginScreen extends Component {
   constructor(props) {
     super(props);
+    console.debug(props)
     this.state = {
     };
   }
 
   render() {
     return (
-      <View>
-        <Text> LoginScreen </Text>
-      </View>
+      <SafeAreaView style = {{flex:1}}>
+        <View style = {{flex:1,alignItems:'center'}}>
+          <Button title = "Login" onPress = {()=>{
+            console.debug("Will Load Home Screen")
+          }}></Button>
+          <Button title = "SignUp" onPress = {()=>{
+            this.props.navigation.push("SignUp")
+          }}></Button>
+          <Button title = "Forget Password" onPress = {()=>{
+            this.props.navigation.push("ForgotPassword")
+          }}></Button>
+        </View>
+      </SafeAreaView>
     );
   }
 }
