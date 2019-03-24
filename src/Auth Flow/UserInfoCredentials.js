0e4ce40cf097191e7f08c8d53035d2ac67d24d5d
Tunvir Rahman <tunvir.rahman@infolytx.com>
2019-03-24 17:37:22 +0600

    On progress

--- src/Auth Flow/UserInfoCredentials.js
+++ src/Auth Flow/UserInfoCredentials.js
@@ -3,52 +3,13 @@ import { View, Text,FlatList } from 'react-native';
 import StepIndicator from 'react-native-step-indicator';
 import Input from "./../UserInput";
 import { Button } from 'react-native-elements';
-
-
-const inputs = {
-  loginCredentials:{
-    Email:{
-      placeholder:"Enter Email",
-      secureText:false
-    },
-    PassWord:{
-      placeholder:"Enter Password",
-      secureText:true
-    },
-    ConfirmPassWord:{
-      placeholder:"Confirm Password",
-      secureText:true
-    },
-  },
-  userInfos:{
-    FirstName:{
-      placeholder:"Enter First Name",
-      secureText:false
-    },
-    LastName:{
-      placeholder:"Enter Last Name",
-      secureText:false
-    },
-    CountryCurrentlyLiving:{
-      placeholder:"Enter Country Name",
-      secureText:false
-    },
-    CountryOfOrigin:{
-      placeholder:"Enter Your birth country Name",
-      secureText:false
-    },
-    Profession:{
-      placeholder:"Enter Your Profession Name",
-      secureText:false
-    }
-  }
-}
+import {formInputs} from "./../Service and Data/FormInfomations";
 
 export default class UserInfoCredentials extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentEntry:"loginCredentials",
+      currentEntry:1,
       title:"Next"
     };
   }
@@ -56,8 +17,8 @@ export default class UserInfoCredentials extends Component {
   render() {
     return (
       <View style = {{flex:1,backgroundColor:'transparent',justifyContent:'flex-start',flexDirection:'column'}}>
-        <FlatList style ={{flex:0.1,backgroundColor:'gray'}} data = {Object.keys(inputs[this.state.currentEntry])} keyExtractor = {(item,index)=>item} renderItem = {({item})=>{
-          return <Input placeholder = {inputs[this.state.currentEntry][item].placeholder}></Input>
+        <FlatList style ={{flex:0.1,backgroundColor:'gray'}} data = {Object.keys(formInputs[this.state.currentEntry])} keyExtractor = {(item,index)=>item} renderItem = {({item})=>{
+          return <Input placeholder = {formInputs[this.state.currentEntry][item].placeholder}></Input>
         }}></FlatList>
         <Button >{this.state.title}</Button>
       </View>
---
+++ src/Service and Data/FormInfomations.js
@@ -0,0 +1,37 @@
+export const formInputs = [
+    {
+        Email:{
+          placeholder:"Enter Email",
+          secureText:false
+        },
+        PassWord:{
+          placeholder:"Enter Password",
+          secureText:true
+        },
+        ConfirmPassWord:{
+          placeholder:"Confirm Password",
+          secureText:true
+        },
+      },{
+        FirstName:{
+          placeholder:"Enter First Name",
+          secureText:false
+        },
+        LastName:{
+          placeholder:"Enter Last Name",
+          secureText:false
+        },
+        CountryCurrentlyLiving:{
+          placeholder:"Enter Country Name",
+          secureText:false
+        },
+        CountryOfOrigin:{
+          placeholder:"Enter Your birth country Name",
+          secureText:false
+        },
+        Profession:{
+          placeholder:"Enter Your Profession Name",
+          secureText:false
+        }
+      }
+]
