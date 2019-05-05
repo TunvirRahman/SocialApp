681bde7d871278c77bf3e63a3b8c30dd5ad03322
Tunvir Rahman <tunvir.rahman@infolytx.com>
2019-05-05 11:00:21 +0600

    V2 in progress.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './source/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
