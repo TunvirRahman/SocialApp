3e4dfed805feddd2fb0f93ccc8b925adccfbe0a1
Tunvir Rahman <tunvir.rahman@infolytx.com>
2019-05-05 11:09:39 +0600

    Hello World to restructure.

--- source/App.js
+++ source/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
   }
 
   render() {
     return (
-      <View>
-        <Text> App </Text>
+      <View style = {styles.rootView}>
+        <Text>Hello World</Text>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  rootView:{
+    flex:1,
+    backgroundColor:'transparent',
+    alignItems: 'center',
+    justifyContent:'center'
+  }
+})
